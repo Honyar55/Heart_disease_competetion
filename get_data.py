fef5fe852f9b843b191314ea05c86d972a21b7b6
Honyar Bahir <honyarbahir5@gmail.com>
2026-03-11 22:57:51 +0300

    Add data setup function to download dataset
    Checks for the existence of 'train.csv' and downloads data from Kaggle if not found.

---
+++ get_data.py
@@ -0,0 +1,11 @@
+import os
+
+def setup_data():
+    if not os.path.exists('train.csv'):
+        print("Data not found. Downloading from Kaggle...")
+        os.system("kaggle competitions download -c playground-series-s6e2")
+        os.system("unzip playground-series-s6e2.zip")
+    else:
+        print("Dataset already present and ready for analysis.")
+
+setup_data()
